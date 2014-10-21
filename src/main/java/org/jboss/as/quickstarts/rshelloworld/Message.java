6829b790daa1f4b40eb591b6517940238b06f067
Lukas Fryc <lukas@fryc.eu>
2014-10-22 00:48:52 +0200

    removed lombok

--- src/main/java/org/jboss/as/quickstarts/rshelloworld/Message.java
+++ src/main/java/org/jboss/as/quickstarts/rshelloworld/Message.java
@@ -1,13 +1,27 @@
 package org.jboss.as.quickstarts.rshelloworld;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
 public class Message {
 
     private String author;
 
     private String text;
+
+    public Message() {
+    }
+
+    public Message(String author, String text) {
+        super();
+        this.author = author;
+        this.text = text;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+
 }
